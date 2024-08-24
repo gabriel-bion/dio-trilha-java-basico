33cf6a91afc4678cea29e949edb4bd94172adde6
Gabriel Bion <98788606+gabriel-bion@users.noreply.github.com>
2024-08-24 20:02:48 -0300

    exemplos de constantes

--- TIPOS-VARIAVES/src/TiposVariaveis.java
+++ TIPOS-VARIAVES/src/TiposVariaveis.java
@@ -1,6 +1,8 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Olá, Mundo!");
-        System.out.println("Teste de commit");
+        
+
+        final double VALOR_DE_PI = 3.14;
+        System.out.println(VALOR_DE_PI);
     }
 }
