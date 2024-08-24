a29752b6e5ce52a6ac4e4ddf4b4f3956ea2411cb
Gabriel Bion <98788606+gabriel-bion@users.noreply.github.com>
2024-08-24 19:42:17 -0300

    novo nome de arquivo

---
+++ TIPOS-VARIAVES/src/TiposVariaveis.java
@@ -0,0 +1,6 @@
+public class TiposVariaveis {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Olá, Mundo!");
+        System.out.println("Teste de commit");
+    }
+}
