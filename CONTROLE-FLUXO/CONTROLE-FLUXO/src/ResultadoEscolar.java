d8c2b2fc1fa8ad90d0ba89d5e8338217e95337d6
Gabriel Bion <98788606+gabriel-bion@users.noreply.github.com>
2024-08-30 22:42:14 -0300

    novos arquivos e atualizações

--- CONTROLE-FLUXO/CONTROLE-FLUXO/src/ResultadoEscolar.java
+++ CONTROLE-FLUXO/CONTROLE-FLUXO/src/ResultadoEscolar.java
@@ -9,9 +9,15 @@ public class ResultadoEscolar {
         else if(nota >= 5 && nota < 7){
             System.out.println("Prova de ecuperação");
         }
-        
+
         else{
             System.out.println("Reprovado");
         }
+
+        /*Operador Ternario
+        int nota = 6;
+        String resultado = nota >= 7 ?"Aprovado" : nota >=5 && nota < 7 "Recuperação": "Reprovado";
+        System.out.println(resultado);
+        */
     }
 }
